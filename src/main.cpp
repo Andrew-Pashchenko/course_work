33c02da4e86ec73a53b163d16c6f24a5de2a79da
Andrew-Pashchenko <sk.ma1lz98@gmail.com>
2018-09-16 18:48:54 +0700

    Add function

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <graphics.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <conio.h>
+#include <locale.h>
+#include <string.h>
+#include "menu.h"
+
+using namespace std;
+
+int main()
+{   
+	initwindow(660,660);
+	setbkcolor(0);
+	menu();
+	return 0;
+}
